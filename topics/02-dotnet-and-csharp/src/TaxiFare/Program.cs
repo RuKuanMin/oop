fdd3ae92ad583039464a4989aa193606d66a3c91
Hung Nguyen <hungdhnt@gmail.com>
2021-10-16 09:00:25 +0700

    update

--- topics/02-dotnet-and-csharp/src/TaxiFare/Program.cs
+++ topics/02-dotnet-and-csharp/src/TaxiFare/Program.cs
@@ -17,8 +17,14 @@ bool inputOK=false;
 
 // Input
 Console.WriteLine("Nhap quang duong di (km):");
-do{// Kiểm tra việc nhập dữ liệu 
-   // -> yêu cầu nhập lại cho đến khi thỏa mãn là giá trị số > 0
+
+/*
+Kiểm soát việc nhập dữ liệu 
+-> yêu cầu nhập lại cho đến khi thỏa mãn là giá trị số > 0
+*/
+do{
+    // Hàm ReadLine() trả về chuỗi ký tự nhập từ bàn phím 
+    // -> cần thực hiện chuyển chuỗi ký tự sang kiểu dữ liệu phù hợp
     inputOK = double.TryParse(Console.ReadLine(), out quangDuong);
     if(!inputOK || quangDuong <= 0) 
         Console.WriteLine("Quang duong phai la so > 0.");
@@ -34,9 +40,4 @@ else    if(quangDuong <= 5)
                 else 
                     soTien = (Gia1 + (quangDuong - 1) * Gia2 + (quangDuong - 5) * Gia3) * 0.9;
 /// Output
-Console.WriteLine("Quang duong: {0} (km), so tien: {1} (VND)", quangDuong, soTien);
-          
-
-
-
-
+Console.WriteLine("Quang duong: {0:0.00} (km), so tien: {1:C} (VND)", quangDuong, soTien);
