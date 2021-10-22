e719f37f5e9f48cdb05b2e401bf33146261596ea
Hung Nguyen <hungdhnt@gmail.com>
2021-10-22 18:49:20 +0700

    Fix logic error on electricity bill calculation

---
+++ topics/02-dotnet-and-csharp/src/ExceptionHandle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExceptionHandle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=0;
+            Console.Write("Nhap n = ");
+            try{
+                n = int.Parse(Console.ReadLine());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Gia tri vua nhap khong hop le.");
+            }
+            finally
+            {
+                Console.WriteLine("So vua nhap la: {0}", n);
+            }
+        }
+    }
+}
